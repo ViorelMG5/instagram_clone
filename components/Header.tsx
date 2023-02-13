81b1bafa97a96b30067be6044672d525742bf8f2
ViorelMG5 <viorel@mg5.ro>
2023-02-13 13:12:59 +0200

    fixed ui bug on search

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
       />
       <div className="relative ml-auto mr-4 text-gray-600">
         <input
-          className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          className="border-2 border-gray-300 bg-white h-10 px-5 pr-4 rounded-lg text-sm focus:outline-none"
           type="search"
           name="search"
           placeholder="Search"
