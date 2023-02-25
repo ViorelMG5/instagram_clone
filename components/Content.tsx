3279e3fe631c98ca036b2ec5cecd9c3f292ff71e
ViorelMG5 <viorel@mg5.ro>
2023-02-25 17:23:59 +0200

    fixed ui bug on stories

--- components/Content.tsx
+++ components/Content.tsx
@@ -4,7 +4,7 @@ import Story from "./Story";
 
 export default function Content() {
   return (
-    <div className="grow-[3] mt-12 md:mt-10">
+    <div className="grow-[3] mt-20 md:mt-10">
       <div className=" max-w-md  mx-auto px-2 mb-20 md:px-0">
         <Story />
         <Feed />
