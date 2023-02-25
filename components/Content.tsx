6181429afcdfc0e5dad2ad4fcb631470514d883c
ViorelMG5 <viorel@mg5.ro>
2023-02-25 17:16:14 +0200

    fixed ui bug on reponsive

--- components/Content.tsx
+++ components/Content.tsx
@@ -5,7 +5,7 @@ import Story from "./Story";
 export default function Content() {
   return (
     <div className="grow-[3] mt-12 md:mt-10">
-      <div className=" max-w-md  mx-auto px-2 md:px-0">
+      <div className=" max-w-md  mx-auto px-2 mb-20 md:px-0">
         <Story />
         <Feed />
       </div>
